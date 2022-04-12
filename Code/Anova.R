7883213f0cfe2e56788c58b8093a000c34ad3991
yw448 <yw448@duke.edu>
2022-04-12 14:54:01 -0400

    Anova analysis

---
+++ Code/Anova.R
@@ -0,0 +1,43 @@
+library(tidyverse)
+library(agricolae)
+mytheme=theme(
+  panel.background = element_rect(fill = "white", colour = "grey50"),
+  legend.position = c(.98, .98),
+  legend.justification = c("right", "top"),
+  legend.box.just = "right",
+  legend.margin = margin(6, 6, 6, 6))
+theme_set(mytheme)
+FiveCountries=read.csv("./Data/Processed/FiveCountries.csv",stringsAsFactors = TRUE)
+# Test for normality
+Year2019=filter(FiveCountries,Year==2019)
+
+shapiro.test(Year2019$Index
+             [Year2019$Country=="Australia"])
+shapiro.test(Year2019$Index
+             [Year2019$Country=="Japan"])
+shapiro.test(Year2019$Index
+             [Year2019$Country=="South Africa"])
+shapiro.test(Year2019$Index
+             [Year2019$Country=="United Kingdom"])
+shapiro.test(Year2019$Index
+             [Year2019$Country=="United States"])
+##results: reject null in all 
+qqnorm(Year2019$Index);qqline(Year2019$Index)
+
+bartlett.test(Year2019$Index ~ Year2019$Country)
+#results: reject null i.e. variances are not equal
+Year2019.anova=aov(data = Year2019, Index~Country)
+summary(Year2019.anova)
+#results: reject null hypothesis 
+#i.e. difference between a pair of group means is statiscally significant
+
+group.2019=HSD.test(Year2019.anova, "Country", group = TRUE)
+group.2019
+group.2019.plot <- ggplot(Year2019, 
+                          aes(x = reorder(Country,-Index), y = Index)) +
+  geom_boxplot() +
+  stat_summary(geom = "text", fun = max, vjust = -1, size = 3.5,
+               label = c("a","a","a","b", "b")) +
+  labs(x = "Country", y = "Impact Intensity")  +
+  ggtitle("2019 Average Company Environmental Impact Intensity")
+print(group.2019.plot)
